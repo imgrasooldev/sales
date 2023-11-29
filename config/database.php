93e0f8a7d81cece94e2ca7ce0d66398c3a38619f
root <root@ip-172-31-12-6.ap-southeast-2.compute.internal>
2023-11-29 20:12:26 +0000

    After Deployment

--- config/database.php
+++ config/database.php
@@ -61,6 +61,10 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'modes' => [
+                // Disable ONLY_FULL_GROUP_BY mode
+                // 'ONLY_FULL_GROUP_BY',
+            ],
         ],
 
         'pgsql' => [
